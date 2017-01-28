c160d821685103164c02a1cd019174fc552f87ee
schore <schore@lenovo.Speedport_W_724V_01011603_00_009>
2017-01-28 16:50:09 +0100

    zwischenstand

---
+++ src/include/debug_helper.h
@@ -0,0 +1,16 @@
+#ifndef DEBUG_HELPER_H_
+#define DEBUG_HELPER_H_
+
+#include <iostream>
+
+#define DEBUG_PRINT(_OUTPUT) \
+  std::cout << __FILE__ << ":" << __LINE__ << std::endl;\
+  std::cout << __OUTPUT << std::endl
+
+#define ASSERT(_ASSUMPTION, _RETURN) \
+  if(_ASSUMPTION) {\
+    DEBUG_PRINT(# _ASSUMPTION);}\
+    return _RETURN;\
+  }
+
+#endif
