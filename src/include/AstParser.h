ae2d5a580a2a08ef0e63efda3efc828a6e7573a6
schore <x@xx.de>
2017-02-05 20:51:00 +0100

    Ast Parser

--- src/include/AstParser.h
+++ src/include/AstParser.h
@@ -5,21 +5,39 @@
 #include "token.h"
 #include "AstElement.h"
 
-
 class Token;
 class AstParser
 {
+  enum ElementType {
+    AstEntry,
+    AstRoot,
+    AstFunc,
+    AstStmt,
+    AstIdList,
+  };
+
+  struct Element {
+    ElementType type;
+    int parent;
+  };
+
 private:
   std::vector <Token *> inStream;
-  int index;
+  std::vector <int> parent;
+  std::vector <Element> node;
 
-  bool checkToken(eTokenType type);
+  int index;
+  int parentIndex;
+
+  int addNode(ElementType type, int par);
+
+  bool checkToken(eTokenType type, int parent);
   bool isNextToken(eTokenType type);
 
-  bool root();
-  bool func();
-  bool stmt();
-  bool idList();
+  bool root(int parent);
+  bool func(int parent);
+  bool stmt(int parent);
+  bool idList(int parent);
 public:
   bool parseToken(std::vector <Token*> inStream);
 
--- src/parser/AstParser.cpp
+++ src/parser/AstParser.cpp
@@ -12,8 +12,9 @@ using namespace std;
 
 AstParser::AstParser() : index(-1) {};
 
-bool AstParser::checkToken(eTokenType type) {
-  cout << "Consumed " << this->index << " ";
+bool AstParser::checkToken(eTokenType type, int parent) {
+  cout << "Consumed " << this->index << " " << parent << " ";
+  this->parent[this->index] = parent;
   this->inStream[this->index]->printToken();
   return (type == this->inStream[this->index++]->getType());
 }
@@ -22,51 +23,79 @@ bool AstParser::isNextToken(eTokenType type) {
   return (type == this->inStream[this->index]->getType());
 }
 
+int AstParser::addNode(ElementType type, int par) {
+  Element e;
+  e.type = type;
+  e.parent = par;
 
-bool AstParser::stmt() {
+  this->node.push_back(e);
+  return this->parentIndex++;
+}
+
+bool AstParser::stmt(int parent) {
+  int par = this->addNode(AstFunc, parent);
   return true;
 }
 
-bool AstParser::idList() {
+bool AstParser::idList(int parent) {
+  int par = this->addNode(AstIdList, parent);
   int safe = this->index;
   bool ret = false;
 
   return       this->isNextToken(Token_BraceRight)
-         ||(   this->checkToken(Token_Identifier)
+         ||(   this->checkToken(Token_Identifier, par)
             && this->isNextToken(Token_BraceRight))
          ||((  this->index = safe,
-               this->checkToken(Token_Identifier)
-            && this->idList()))
+               this->checkToken(Token_Identifier, par)
+            && this->idList(par)))
          ||((  this->index = safe,
-               this->checkToken(Token_Comma)
-            && this->checkToken(Token_Identifier)
-            && this->idList()));
+               this->checkToken(Token_Comma, par)
+            && this->checkToken(Token_Identifier, par)
+            && this->idList(par)));
 }
 
-bool AstParser::func() {
+bool AstParser::func(int parent) {
+  int par = this->addNode(AstFunc, parent);
   int safe = this->index;
-  bool ret =  this->checkToken(Token_KeywordFunc)
-          &&  this->checkToken(Token_Identifier)
-          &&  this->checkToken(Token_BraceLeft)
-          &&  this->idList()
-          &&  this->checkToken(Token_BraceRight)
-          &&  this->checkToken(Token_SwiftLeft)
-          &&  this->stmt()
-          &&  this->checkToken(Token_SwiftRight);
+  bool ret =  this->checkToken(Token_KeywordFunc, par)
+          &&  this->checkToken(Token_Identifier, par)
+          &&  this->checkToken(Token_BraceLeft, par)
+          &&  this->idList(par)
+          &&  this->checkToken(Token_BraceRight, par)
+          &&  this->checkToken(Token_SwiftLeft, par)
+          &&  this->stmt(par)
+          &&  this->checkToken(Token_SwiftRight, par);
 }
 
-bool AstParser::root() {
+bool AstParser::root(int parent) {
+  int par = this->addNode(AstRoot, parent);
   int safe = this->index;
-  return (   this->checkToken(Token_Eof)
+  return (   this->checkToken(Token_Eof, par)
           || (this->index = safe,
-              this->func() && this->root()));
+              this->func(par) && this->root(par)));
 
 }
 
 bool AstParser::parseToken(vector <Token *> inStream) {
   this->inStream = inStream;
   this->index = 0;
+  this->parentIndex = 0;
+  this->parent.resize(inStream.size(), 0);
 
-  this->root();
+  int par = this->addNode(AstEntry, -1);
+
+  this->root(par);
+
+  for ( Element n : this->node) {
+   cout << n.type << " " << n.parent << endl;
+  }
+
+  cout << endl;
+
+  int i;
+  for ( int j : parent) {
+    cout << i++ << " " << j << endl;
+  }
+
   return false;
 }
